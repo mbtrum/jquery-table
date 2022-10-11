83c588b2f075a2f28e4ef9efbd35bf8b1acb39e0
Michael Trumbull <michael.trumbull@nscc.ca>
2022-10-10 23:43:53 -0300

    Adding formatting and few more features

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Use this variable to rack which row in the table is currently selected
 let currentIndex = 1
 
 const persons = [
@@ -12,6 +13,9 @@ const persons = [
     {
         FirstName: "Bubba",
         LastName: "Fett"
+    },{
+        FirstName: "Luke",
+        LastName: "Skywalker"
     },
     {
         FirstName: "Leia",
@@ -19,10 +23,11 @@ const persons = [
     }
 ]
 
-
-
 $(document).ready(function () {
-    // Attach event to table when row is clicked. Change the current selected row and display update
+    //
+    // Attach click event to table when row is clicked. 
+    // Change the current selected row (currentIndex variable) and display current row to user
+    //
     $(".table").on("click", "td", function () {
         var index = $(this).parent().index('tr');
         currentIndex = index
@@ -30,7 +35,9 @@ $(document).ready(function () {
         DisplayCurrentSelection()
     });
 
+    //
     // Load the table
+    //
     let row_count = 1
 
     // Populate the track list table
@@ -48,9 +55,17 @@ $(document).ready(function () {
     DisplayCurrentSelection()
 });
 
+// Display info for the currentIndex
 function DisplayCurrentSelection() {
     let index = currentIndex - 1 // javascript arrays are 0 based
+ 
     let  display = "Selected Index: " + currentIndex
     display += "<br>" + persons[index].FirstName + " " + persons[index].LastName
+ 
     $("#selected").html(display)
+
+    // Style current row in playlist
+    $("tr").removeClass() 
+
+    $("tr:eq('" + currentIndex + "')").addClass('selected-row')
 }
